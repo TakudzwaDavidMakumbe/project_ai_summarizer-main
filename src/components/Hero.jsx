685ce28bff512e704681f8fb97bb2b817b860ed7
TakudzwaDavidMakumbe <takuclarity@gmail.com>
2025-03-16 15:38:36 +0200

    update Hero component button link to new project repository

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/TakudzwaDavidMakumbe/project_ai_summarizer-main.git", "_blank")
           }
           className='black_btn'
         >
